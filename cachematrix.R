ea3d2ba619e0f5d59f2fc467d287f1989a2e25ec
patricialrt <patricia.bohl@gmail.com>
2015-07-17 21:54:25 +0200

    add some more comments to better understand the homework

--- cachematrix.R
+++ cachematrix.R
@@ -1,17 +1,22 @@
 ## The assignment is to write a pair of functions that cache the inverse of a matrix. 
-
 ## To achieve this I copied the example (makeVector and cacheMean) 
 ## and adapted it to the purpose of the assignment. 
 
+
+## Function 1: makeCacheMatrix:
 ## setting and getting the value of the matrix
 ## setting and getting the value of the inversion
 
 makeCacheMatrix <- function(x=matrix()){
-  mInv <- NULL
-  set <- function(y) {
-    x <<- y
+	
+  # store in here the result of the inversion:
+  mInv <- NULL  		
+  
+  # setter function, to set matrix to object created by makeCacheMatrix:
+  set <- function(y) {	    x <<- y
     mInv <<- NULL
   }
+  # return input matrix, set and get the inversed matrix and return a list containing these functions
   get <- function() x
   setInv <- function(inv) mInv <<- inv
   getInv <- function () mInv
@@ -21,16 +26,20 @@ makeCacheMatrix <- function(x=matrix()){
 }
 
 
+## Function 2: cacheSolve: 
 ## checking whether the inversion has already been calculated 
-##    - if yes and the matrix has not been  changed, skip computation and prompt message 'getting cached data' 
-##      and retrieve the inverse from the cache
+##    - if yes and the matrix has not been changed: skip computation, retrieve and return the inverse from the cache
 ##    - if no calculate inversion of the matrix and set the inversion in the cache via setInv
+
   cacheSolve <-  function(x, ...) {
+  	# get the inversed matrix from x
     m <- x$getInv()
+    # if available return calculated version
     if(!is.null(m)) {
       message("getting cached data")
       return(m)
     }
+    # if not get x, solve, set and return the result
     data <- x$get()
     m <- solve(data)
     x$setInv(m)
